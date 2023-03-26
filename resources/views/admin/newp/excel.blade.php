a4d646963e009891e57d933d6d8cc251ba912f2e
dmitriyl99 <dmitriyl1899@gmail.com>
2023-03-26 21:04:25 +0500

    Add price and customer

--- resources/views/admin/newp/excel.blade.php
+++ resources/views/admin/newp/excel.blade.php
@@ -1,10 +1,12 @@
 <table>
     <thead>
         <tr>
-            <th>Название</th>    
+            <th>Название</th>
             <th>Тип платежа</th>
             <th>Дата создание</th>
             <th>Количество</th>
+            <th>Цена</th>
+            <th>Пользователь</th>
         </tr>
     </thead>
     <tbody>
@@ -22,6 +24,8 @@
                                 <td>
                                     {{($item->count ?? '')}}
                                 </td>
+                                <td>{{ $item->listproduct->price_3 ?? ''}}</td>
+                                <td>{{ $item->customer != null ? empty($item->customer->full_name) ? $item->customer->phone : $item->customer->full_name : $item->customer_id }}</td>
                             </tr>
                             @endforeach
     </tbody>
